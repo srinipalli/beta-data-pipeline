a297a61ae00d51a1c7858fd6655433c7d3ab9264
Adarsh Siva <mbas@Mbas-MacBook-Pro.local>
2025-06-07 20:41:51 +0530

    Initial push data pipeline experimental

--- csv_input/cleancsv.py
+++ csv_input/cleancsv.py
@@ -1,4 +1,4 @@
 import pandas as pd
 C = input("Enter your csv file relative path: ")
 df = pd.read_csv(C)
-df.to_csv(f'csv_input/cleaned.csv', index=False, quoting=1)  # quoting=1 = QUOTE_ALL
+df.to_csv(f'csv_input/cleaned.csv', index=False, quoting=1)
